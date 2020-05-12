3c14abd445a1ad90632dd9cabe472e6a2bbdfcaf
Jitgitbit <rowowolf@gmail.com>
2020-05-12 14:30:58 +0200

    Using Subjects !

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({providedIn: 'root'})                 // <--- same as registering in app.module !
 export class UserService {
-  activatedEmitter = new EventEmitter<boolean>();
+  activatedEmitter = new Subject<boolean>();          // a subject is active, as opposed to a Observable(which is Passive) !
 }
