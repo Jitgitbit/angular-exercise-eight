1f217810f86e4a2aa0a4dde13fe693696f4b0d5f
Jitgitbit <rowowolf@gmail.com>
2020-05-12 12:36:27 +0200

    Building a Custom Observable (and semi)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +9,30 @@ import { interval, Subscription } from 'rxjs';
 export class HomeComponent implements OnInit, OnDestroy {
 
   private firstObsSubscription: Subscription
+  private secondObsSubscription: Subscription
 
   constructor() { }
 
   ngOnInit() {
     this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);                                    // MEMORY LEAK !!!!! DANGER DANGER !
+      console.log(`semi-custom Observable says what?`,count);                                    // MEMORY LEAK !!!!! DANGER DANGER !
+    });
+
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(()=>{
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.secondObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(`CUSTOM Observable says what?`,data);
     });
   }
 
   ngOnDestroy(): void {
-    this.firstObsSubscription.unsubscribe();                 // now when you navigate away, the interval stops, no more memory leak
+    this.firstObsSubscription.unsubscribe();                 // now when you navigate away, the interval stops, no more memory leak !
+    this.secondObsSubscription.unsubscribe();
   }
 }
