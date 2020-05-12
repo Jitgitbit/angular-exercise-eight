f8671214c474c2fd0c2c38b9914283f4f0ad89f3
Jitgitbit <rowowolf@gmail.com>
2020-05-12 13:14:19 +0200

    completion handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   private firstObsSubscription: Subscription
   private secondObsSubscription: Subscription
+  private thirdObsSubscription: Subscription
 
   constructor() { }
 
@@ -22,10 +23,23 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(()=>{
         observer.next(count);
+        if(count === 9){
+          observer.complete();
+        }
         if(count > 7){
           observer.error(new Error('==> Oh no, count is greater than 7 !'));
         }
         count++;
+      }, 1000);
+    });
+    const customIntervalObservableForComplete = Observable.create(observer => {
+      let ocount = 0;
+      setInterval(()=>{
+        observer.next(ocount);
+        if(ocount === 11){
+          observer.complete();
+        }
+        ocount++;
       }, 1000);
     });
 
@@ -35,10 +49,16 @@ export class HomeComponent implements OnInit, OnDestroy {
       console.log(`==> error log says what?`,error);
       alert(error.message);
     });
+    this.thirdObsSubscription = customIntervalObservableForComplete.subscribe(odata => {
+      console.log(`CUSTOM Observable for completion says what?`,odata);
+    }, () => {
+      console.log(`Completed!!!`);
+    });
   }
 
   ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();                 // now when you navigate away, the interval stops, no more memory leak !
     this.secondObsSubscription.unsubscribe();
+    this.thirdObsSubscription.unsubscribe();
   }
 }
